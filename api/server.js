a230d3666737c83c56b87e16aebb6c76de824cbc
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-05-27 19:50:31 +0200

    Format and lint

--- api/server.js
+++ api/server.js
@@ -1,43 +1,42 @@
 const express = require("express");
 const app = express();
 //enable CORS 
-var cors = require('cors');
+const cors = require('cors');
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({origin: true, credentials: true}));
 
 app.get("/", (req, res) => {
-    res.send("Hello World from express?!");
+	res.send("Hello World from express?!");
 });
 
 app.get("/timeline", (req, res) => {
-    const TIME_SLOT_SIZE_MINUTES = 5;
-    const AMOUNT_WARNING = 8;
-    const AMOUNT_CRITICAL = 10;
-    const COLOR_OK = '#0ce504';
-    const COLOR_WARN = '#e59604';
-    const COLOR_CRIT = '#B33';
+	const TIME_SLOT_SIZE_MINUTES = 5;
+	const AMOUNT_WARNING = 8;
+	const AMOUNT_CRITICAL = 10;
+	const COLOR_OK = '#0ce504';
+	const COLOR_WARN = '#e59604';
+	const COLOR_CRIT = '#B33';
 
-    const timeSlots = [];
-    const currentTime = new Date();
-    const minutes = currentTime.getMinutes();
-    currentTime.setMinutes(minutes - (minutes % TIME_SLOT_SIZE_MINUTES));
-    for (var i = -5; i < 15; i++) {
-        // Generate time slots every TIME_SLOT_SIZE_MINUTES minutes
-        timeSlots.push({ time: currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) });
-        currentTime.setMinutes(currentTime.getMinutes() + TIME_SLOT_SIZE_MINUTES);
-    }
+	const timeSlots = [];
+	const currentTime = new Date();
+	const minutes = currentTime.getMinutes();
+	currentTime.setMinutes(minutes - (minutes % TIME_SLOT_SIZE_MINUTES));
+	for (let i = -5; i < 15; i++) {
+		// Generate time slots every TIME_SLOT_SIZE_MINUTES minutes
+		timeSlots.push({time: currentTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})});
+		currentTime.setMinutes(currentTime.getMinutes() + TIME_SLOT_SIZE_MINUTES);
+	}
 
-    const heights = timeSlots.map((timeSlot, index) => parseInt(Math.random() * 15));
+	const heights = timeSlots.map((timeSlot, index) => Math.random() * 15);
 
 
-
-    res.send(timeSlots.map((timeSlot, index) => ({
-        time: timeSlot.time,
-        height: heights[index],
-        color: heights[index] > AMOUNT_CRITICAL ? COLOR_CRIT : (heights[index] > AMOUNT_WARNING ? COLOR_WARN : COLOR_OK),
-        border: index == 5 ? '#000' : '#FFF',
-        width: index == 5 ? 4 : 0,
-    })));
+	res.send(timeSlots.map((timeSlot, index) => ({
+		time: timeSlot.time,
+		height: heights[index],
+		color: heights[index] > AMOUNT_CRITICAL ? COLOR_CRIT : (heights[index] > AMOUNT_WARNING ? COLOR_WARN : COLOR_OK),
+		border: index === 5 ? '#000' : '#FFF',
+		width: index === 5 ? 4 : 0,
+	})));
 });
 
 const port = process.env.PORT || 3000;
