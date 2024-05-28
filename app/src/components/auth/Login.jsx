3ca5ea1c538b436505574c040f6f089a8a982fa3
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-05-28 16:01:31 +0200

    Implement toy authentication

--- app/src/components/auth/Login.jsx
+++ app/src/components/auth/Login.jsx
@@ -1,17 +1,47 @@
 import './Login.css';
+import {redirect} from "react-router-dom";
+import {useState} from "react";
 
 const Login = () => {
-	return (
-		<div className="content">
-			<form>
-				<label htmlFor="username">Username:</label>
-				<input type="text" id="username" name="username"/>
-				<label htmlFor="password">Password:</label>
-				<input type="password" id="password" name="password"/>
-				<button type="submit">Login</button>
-			</form>
-		</div>
-	);
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const username = event.target.username.value;
+
+        fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username}),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Login failed');
+                }
+            })
+            .then((data) => {
+                localStorage.setItem('token', data.token);
+                redirect('/');
+            }).catch((error) => {
+            setErrorMessage(error.message);
+        });
+    }
+
+
+    return (
+        <div className="content">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username"/>
+                <button type="submit">Login</button>
+                {errorMessage && <p>{errorMessage}</p>}
+            </form>
+        </div>
+    );
 }
 
 export default Login;
