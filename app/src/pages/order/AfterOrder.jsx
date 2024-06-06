ddd2d491a9a4c5e47675593e57e12dcd7fda2107
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-06-06 20:59:28 +0200

    Add QR code to show order

--- app/src/pages/order/AfterOrder.jsx
+++ app/src/pages/order/AfterOrder.jsx
@@ -1,15 +1,34 @@
+import {useEffect, useRef} from 'react';
 import {Link, useParams} from "react-router-dom";
+import QRCode from 'qrcode';
 
 const AfterOrder = () => {
-    const { orderNumber } = useParams();
+	const {orderNumber} = useParams();
+	const canvasRef = useRef(null);
 
-    return (
-        <div>
-            <h1>Thank you for your order!</h1>
+	// Link to the order page
+	const orderLink = `/order/${orderNumber}`;
 
-            <p>Your order number is: <Link to={`/order/${orderNumber}`}>{orderNumber}</Link></p>
-        </div>
-    )
+	useEffect(() => {
+		if (orderNumber) {
+			QRCode.toCanvas(canvasRef.current, orderLink, {errorCorrectionLevel: 'H'}, (error) => {
+				if (error) console.error(error);
+			});
+		}
+	}, [orderNumber]);
+
+
+	return (
+		<div className="flex flex-col items-center p-4 bg-gray-100 rounded-lg shadow-md">
+			<h1 className="text-2xl font-bold mb-4 text-center text-gray-800">Thank you for your order!</h1>
+
+			<canvas ref={canvasRef} className="border rounded-lg mb-4"></canvas>
+
+			<p className="text-lg text-gray-700">
+				Your order number is: <Link to={orderLink} className="text-blue-500 hover:underline">{orderNumber}</Link>
+			</p>
+		</div>
+	);
 }
 
 export default AfterOrder;
