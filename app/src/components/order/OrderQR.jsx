5a113cbaf468071f7800a370bc071dfc74b744d4
Zeilenschubser <zeilenschubser@gmail.com>
2024-06-08 19:56:01 +0200

    fixed a small bug with the QR-Code link

--- app/src/components/order/OrderQR.jsx
+++ app/src/components/order/OrderQR.jsx
@@ -1,24 +1,25 @@
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 import QRCode from "qrcode";
 
 const OrderQR = (orderId) => {
-	const canvasRef = useRef(null);
+    const canvasRef = useRef(null);
 
-	// Link to the order page
-	const orderLink = `/order/${orderId}`;
+    // Link to the order page
+    const orderLink = `/order/${orderId.orderId}`;
+    console.log("orderID in OrderQR:", orderId.orderId)
 
-	// Generate QR code
-	useEffect(() => {
-		if (orderId) {
-			QRCode.toCanvas(canvasRef.current, orderLink, {errorCorrectionLevel: 'H'}, (error) => {
-				if (error) console.error(error);
-			});
-		}
-	}, [orderId, orderLink]);
+    // Generate QR code
+    useEffect(() => {
+        if (orderId) {
+            QRCode.toCanvas(canvasRef.current, orderLink, { errorCorrectionLevel: 'H' }, (error) => {
+                if (error) console.error(error);
+            });
+        }
+    }, [orderId, orderLink]);
 
-	return (
-		<canvas ref={canvasRef} className="border rounded-lg mb-4"></canvas>
-	)
+    return (
+        <canvas ref={canvasRef} className="border rounded-lg mb-4"></canvas>
+    )
 }
 
 export default OrderQR;
