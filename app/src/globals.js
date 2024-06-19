3410f2b0ecbef9c8d51c16d502037c3615b9f096
Zeilenschubser <zeilenschubser@gmail.com>
2024-06-19 19:23:45 +0200

    globals had wrong name

--- app/src/globals.js
+++ app/src/globals.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT  = process.env.API_ENDPOINT  || import.meta.env.VITE_API_ENDPOINT  || "http://localhost:3000";
-export const THIS_ENDPOINT = process.env.THIS_ENDPOINT || import.meta.env.VITE_THIS_ENDPOINT || "http://localhost:8000";
+export const API_ENDPOINT  = process.env.VITE_API_ENDPOINT  || import.meta.env.VITE_API_ENDPOINT  || "http://localhost:3000";
+export const THIS_ENDPOINT = process.env.VITE_THIS_ENDPOINT || import.meta.env.VITE_THIS_ENDPOINT || "http://localhost:8000";
