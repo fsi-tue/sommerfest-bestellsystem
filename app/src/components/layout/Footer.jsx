706f75178671274fc6df50e9b4cd71489d83522b
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-06-21 23:31:26 +0200

    Add link to Zeilenschubser

--- app/src/components/layout/Footer.jsx
+++ app/src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<footer className="border-primary border-2 p-4 flex flex-col justify-between items-center">
 			<p>Copyright © 2024 <a href="https://www.fsi.uni-tuebingen.de">Fachschaft Informatik Tübingen</a></p>
-			<p>Made with 🍕 by Zeilenschubser and Josef Müller</p>
+			<p><a href="https://github.com/Zeilenschubser/">Made with 🍕 by Zeilenschubser and Josef Müller</a></p>
 		</footer>
 	);
 };
