5b9faa7fcd1acf255876a585a6acf858ad71b5cf
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-06-22 16:53:24 +0200

    Add error message component

---
+++ app/src/components/ErrorMessage.jsx
@@ -0,0 +1,10 @@
+export const ErrorMessage = ({error}) => {
+	return (
+		<div className="text-red-500 text-center">
+			{error}
+			<p className="text-sm text-gray-500">
+				Please reload the page and try again.
+			</p>
+		</div>
+	);
+}
