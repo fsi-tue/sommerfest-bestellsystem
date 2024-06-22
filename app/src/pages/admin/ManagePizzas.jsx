a97c4071f78e42ba2b8fe4ac16211eea3bad9225
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-06-22 16:53:57 +0200

    Allow editing of pizzas

--- app/src/pages/admin/ManagePizzas.jsx
+++ app/src/pages/admin/ManagePizzas.jsx
@@ -1,25 +1,56 @@
-import { useEffect, useState } from "react";
-import { API_ENDPOINT, THIS_ENDPOINT } from "../../globals.js";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {API_ENDPOINT} from "../../globals.js";
+import Pizza from "./Pizza.jsx";
+import {ErrorMessage} from "../../components/ErrorMessage.jsx";
 
 const ManagePizzas = () => {
-    //this wont deter anyone
-    const token  = localStorage.getItem('token') || "";
-    const authed = token !== "";
-    
-    const navigate = useNavigate();
-    if(!authed)
-    {
-        navigate("/");
-    }
+	const token = localStorage.getItem('token') || "";
+	const authed = token !== "";
 
+	const navigate = useNavigate();
+	if (!authed) {
+		navigate("/");
+	}
 
-    return (
-        <div className="content">
-            <h2>Manage Pizzas</h2>
-        </div >
-    );
+	const [pizzas, setPizzas] = useState([]);
+	const [error, setError] = useState('');
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch(API_ENDPOINT + '/pizzas/', {
+			headers: {
+				'Authorization': `Bearer ${token}`,
+			}
+		})
+			.then(response => response.json())
+			.then(data => {
+				setPizzas(data);
+				setLoading(false);
+			})
+			.catch(error => {
+				setError('Error fetching pizzas');
+				setLoading(false);
+			});
+	}, [token]);
+
+	if (loading) return <div>Loading...</div>;
+	if (error) return (<ErrorMessage error={error}/>);
+
+	return (
+		<div className="content">
+			<div className="p-4">
+				<h2 className="text-2xl mb-4">Manage Pizzas 🍕</h2>
+			</div>
+
+			<div className="flex flex-col space-y-4">
+				{pizzas.map(pizza => (
+					<Pizza key={pizza._id} pizza={pizza}/>
+				))
+				}
+			</div>
+		</div>
+	);
 }
-
 
 export default ManagePizzas;
