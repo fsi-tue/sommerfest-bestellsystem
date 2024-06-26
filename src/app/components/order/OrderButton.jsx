01706af3892bebc930837be460274546ee6b94e3
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-06-26 11:07:31 +0200

    Add order button debounce

--- src/app/components/order/OrderButton.jsx
+++ src/app/components/order/OrderButton.jsx
@@ -1,29 +1,30 @@
-// Button to order the pizzas
 import PropTypes from "prop-types";
 import {useRouter} from "next/navigation";
+import {useState} from "react";
 import {addToLocalStorage, getFromLocalStorage} from "../../../lib/localStorage";
 
 const OrderButton = ({order}) => {
-	const router = useRouter()
+	const router = useRouter();
+	const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
 	const items = order.items;
 	const name = order.name;
-	const timeslot = order.timeslot
-	const comment = order.comment
+	const comment = order.comment;
 
 	if (items.length === 0) {
-		return;
+		return null;
 	}
 
 	const body = {
 		name: name,
 		pizzas: items,
-		timeslot: timeslot,
 		comment: comment
-	}
+	};
 
 	// Function to order the pizzas
 	const orderPizza = () => {
+		setIsButtonDisabled(true);
+
 		fetch('/api/order', {
 			method: 'POST',
 			headers: {
@@ -34,33 +35,37 @@ const OrderButton = ({order}) => {
 			.then(response => response.json())
 			.then(data => {
 				if (data.orderId) {
-					// console.log(`Order ID: ${data.orderId}`)
-
 					// Add order ID to local storage
 					const orderIds = JSON.parse(getFromLocalStorage('orderIds')) || [];
 					orderIds.push(data.orderId);
 					addToLocalStorage('orderIds', JSON.stringify(orderIds));
 
 					// Redirect to thank you page
-					router.push(`/order/thank-you/${data.orderId}`)
+					router.push(`/order/thank-you/${data.orderId}`);
 				}
 			})
-	}
+			.finally(() => {
+				setIsButtonDisabled(false);
+			});
+	};
 
 	return (
-		<button onClick={() => orderPizza()}
-		        className="bg-primary-950 text-white px-4 py-2 rounded-lg mt-4 w-full md:w-auto hover:bg-primary-800">
+		<button
+			onClick={() => orderPizza()}
+			className={`bg-primary-950 text-white px-4 py-2 rounded-lg mt-4 w-full md:w-auto hover:bg-primary-800 ${isButtonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+			disabled={isButtonDisabled}
+		>
 			Order now
 		</button>
 	);
-}
+};
 
 OrderButton.parameters = {
 	order: {}
-}
+};
 
 OrderButton.propTypes = {
-	order: PropTypes.object
-}
+	order: PropTypes.object.isRequired
+};
 
 export default OrderButton;
