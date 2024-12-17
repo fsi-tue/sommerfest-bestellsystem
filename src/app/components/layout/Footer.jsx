559121f4b4b43dfb91c570d480a5a9a6df275925
Josef Müller <46693545+am9zZWY@users.noreply.github.com>
2024-12-17 17:13:31 +0100

    Update Footer.jsx

--- src/app/components/layout/Footer.jsx
+++ src/app/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 		<footer
 			className="flex flex-col justify-between items-center bg-gray-50 shadow z-10 rounded-lg p-2 md:p-4 my-5 w-full max-w-7xl mx-auto">
 			<p>Copyright © 2024 <a href="https://www.fsi.uni-tuebingen.de">Fachschaft Informatik Tübingen</a></p>
-			<p><a href="https://github.com/Zeilenschubser/">Made with 🍕 by Zeilenschubser and Josef Müller</a></p>
+			<p>Made with 🍕 by <a href="https://github.com/Zeilenschubser/">Zeilenschubser</a> and <a href="https://github.com/am9zZWY">Josef Müller</a></p>
 		</footer>
 	);
 };
